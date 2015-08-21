a346eede2cbd4bc848c0dd0ac32cd5bd986ce331
Renārs Vilnis <renars.vilnis@gmail.com>
2015-08-21 18:04:41 +0300

    Added config and initial config properties

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-module.exports = {};
+var config = {};
+
+/**
+ * Url protocol that gets registered in operating systems
+ * and used to communicate beetween browsers extensions and native application
+ * @type {String}
+ */
+config.urlProtocol = 'lolbuilder';
+
+/**
+ * List of League of Legends build guide website url's that are supported
+ * @type {Array}
+ */
+config.supportedBuildSites = [
+  'probuilds.net/guide/',
+  'mobafire.com/league-of-legends/build/',
+  'solomid.net/guide/view/',
+  'lolpro.com/user-guides/',
+  'lolpro.com/guides/'
+];
+
+module.exports = config;
